43d9ca2130b74615406b6a45852aa7cd01991805
Aleks B <aleks.balev7899@gmail.com>
2023-08-03 10:16:27 +0200

    harpoon fixed

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -38,9 +38,10 @@ return {
   },
   --stylua: ignore
   keys = {
-    { "<leader>cgd", function() require("neogen").generate() end, desc = "Annotation", },
+    { "<leader>cgd", function() require("neogen").generate() end,                  desc = "Annotation", },
     { "<leader>cgc", function() require("neogen").generate { type = "class" } end, desc = "Class", },
-    { "<leader>cgf", function() require("neogen").generate { type = "func" } end, desc = "Function", },
-    { "<leader>cgt", function() require("neogen").generate { type = "type" } end, desc = "Type", },
+    { "<leader>cgf", function() require("neogen").generate { type = "func" } end,  desc = "Function", },
+    { "<leader>cgt", function() require("neogen").generate { type = "type" } end,  desc = "Type", },
   },
 }
+
